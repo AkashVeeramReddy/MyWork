c964be97de64d90a5d9c1aa300cce6745b05da36
nithinjs88 <nithinjs.88@gmail.com>
2015-07-05 22:59:44 +0530

    create full bt from preorder,postorder

---
+++ src/tree/TestBinaryTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class TestBinaryTree {
+
+	public static void main(String[] args) {
+		BinaryTree<Integer> binaryTree = new BinaryTree<Integer>();
+		Integer[]  preOrder = new Integer[]{1, 2, 4, 8, 9, 5, 3, 6, 7};
+		Integer[]  postOrder = new Integer[]{8, 9, 4, 5, 2, 6, 7, 3, 1};
+		binaryTree.populateFullTreeFromPreorderPostOrder(preOrder, postOrder);
+		binaryTree.showImage();
+	}
+
+}
